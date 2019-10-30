522cdb627e0ebf495937d62b9b69c156469009a3
Charles DeLeo <flatironschool@Flatirons-MacBook-Air.local>
2019-10-30 15:50:15 -0400

    defined first three methods

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,39 @@
-# your code goes here
+require 'pry'
+class Person
+    attr_accessor :bank_account, :happiness, :hygiene 
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @hygiene = 8
+        @happiness = 8
+        @bank_account = 25
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def bank_account
+        @bank_account
+    end
+    
+    def happiness=(happiness_level)
+        @happiness= happiness_level
+    end
+
+    def happy?
+        if @happiness > 7
+            true
+        else
+            false
+            # binding.pry
+        end
+    end
+
+    # def clean?
+    # end
+
+end
